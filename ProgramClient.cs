033d06d7fa3554af2c2cc89860ac23b2cff842fd
tamoznich <abramwork@gmail.com>
2023-09-09 13:42:08 +0300

    добавил UDP протокол, хз как их менять пока

--- ProgramClient.cs
+++ ProgramClient.cs
@@ -9,37 +9,73 @@ namespace client_server
     {
         static void Main(string[] args)
         {
+            #region TCP
+            //const string ip = "127.0.0.1"; //выбрал айпи, чтобы реализовывалось на одном компе
+            //const int port = 8080; //выбрал порт
+
+            //var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);//сделал класс эндпоинт,котрый показывает куда смогут подключаться, сделал точку 
+
+            //var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//набор параметров для тсп протокола
+
+            //Console.WriteLine("Введите слова");
+            //var message = Console.ReadLine(); //что мы будем отправлять
+
+            //var data = Encoding.UTF8.GetBytes(message);//кодируем сообщение
+            //tcpSocket.Connect(tcpEndPoint);// делаем подключение нашему сокету, обратаная задача слушанья в сервере
+            //tcpSocket.Send(data); // отправляем наше сообщение
+
+            //var buffer = new byte[200];
+            //var size = 0;
+            //var answer = new StringBuilder(); // ответ с сервера
+
+            //do //цикл с пост условием
+            //{
+            //    size = tcpSocket.Receive(buffer);
+            //    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));//собственно, раскодирую полученные байты в куски сообщений. Использую кодировку UTF8
+            //}
+            //while (tcpSocket.Available > 0); //считывает, пока есть данные
+
+            //Console.WriteLine(answer.ToString());//вывожу ответ сервера
+
+            //tcpSocket.Shutdown(SocketShutdown.Both);//закрываем соединение двух сокетов
+            //tcpSocket.Close();
+
+            //Console.ReadLine();
+            #endregion
+
+            #region UDP
             const string ip = "127.0.0.1"; //выбрал айпи, чтобы реализовывалось на одном компе
-            const int port = 8080; //выбрал порт
+            const int port = 8082;
 
-            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);//сделал класс эндпоинт,котрый показывает куда смогут подключаться, сделал точку 
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);//сделал переменную класса эндпоинт,котрый показывает куда смогут подключаться, сделал точку 
 
-            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//набор параметров для тсп протокола
-
-            Console.WriteLine("Введите слова");
-            var message = Console.ReadLine(); //что мы будем отправлять
-
-            var data = Encoding.UTF8.GetBytes(message);//кодируем сообщение
-            tcpSocket.Connect(tcpEndPoint);// делаем подключение нашему сокету, обратаная задача слушанья в сервере
-            tcpSocket.Send(data); // отправляем наше сообщение
-
-            var buffer = new byte[200];
-            var size = 0;
-            var answer = new StringBuilder(); // ответ с сервера
-
-            do //цикл с пост условием
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//набор параметров для udp протокола
+            udpSocket.Bind(udpEndPoint);    
+            
+            while (true)
             {
-                size = tcpSocket.Receive(buffer);
-                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));//собственно, раскодирую полученные байты в куски сообщений. Использую кодировку UTF8
+                Console.WriteLine("ВВедите сообщение");
+                var message = Console.ReadLine();
+
+                var serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);//сделал переменную класса эндпоинт,вручную прописал куда подключаться
+                udpSocket.SendTo(Encoding.UTF8.GetBytes(message), serverEndPoint); //отправляем данные на конкретный заранее определенный порт
+                
+                var buffer = new byte[256]; // байтовый массив, буфер
+                var size = 0; //сколько реально байт в сообщении получили
+                var data = new StringBuilder(); // будет собирать полученные данные
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8081);// создал экзэмпляр адреса эндпоинт, в который будет записан адрес клиента, который мы будем прослушивать
+
+                do
+                {
+                    size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);
+                    data.Append(Encoding.UTF8.GetString(buffer));
+                }
+                while (udpSocket.Available > 0);
+
+                Console.WriteLine(data);
+                Console.ReadLine();
+                #endregion
             }
-            while (tcpSocket.Available > 0); //считывает, пока есть данные
-
-            Console.WriteLine(answer.ToString());//вывожу ответ сервера
-
-            tcpSocket.Shutdown(SocketShutdown.Both);//закрываем соединение двух сокетов
-            tcpSocket.Close();
-
-            Console.ReadLine();
         }
     }
 }
--- ProgramServer.cs
+++ ProgramServer.cs
@@ -9,36 +9,67 @@ namespace server_client
     {
         static void Main(string[] args)
         {
+            #region TCP
+            //const string ip = "127.0.0.1"; //выбрал айпи, чтобы реализовывалось на одном компе
+            //const int port = 8080;
+
+            //var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);//сделал переменную класса эндпоинт,котрый показывает куда смогут подключаться, сделал точку 
+
+            //var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//набор параметров для тсп протокола
+            //tcpSocket.Bind(tcpEndPoint); //сказал сокету что ему слушать - режим ожидания
+            //tcpSocket.Listen(2); //сколько клиентов может в очередь залезть
+
+            //while (true) //пишу процесс бесконечного прослушивания
+            //{
+            //    var listener = tcpSocket.Accept(); //для каждого клиента будет свой listener, который стирается, когда клиент получил всю запрашиваемую инфу
+            //    var buffer = new byte[256]; // байтовый массив, буфер
+            //    var size = 0; //сколько реально байт в сообщении получили
+            //    var data = new StringBuilder(); // будет собирать полученные данные
+
+            //    do //цикл с пост условием
+            //    {
+            //        size = listener.Receive(buffer);
+            //        data.Append(Encoding.UTF8.GetString(buffer, 0,size));//собственно, раскодирую полученные байты в куски сообщений. Использую кодировку UTF8
+            //    }
+            //    while (listener.Available > 0); //считывает, пока есть данные
+
+            //    Console.WriteLine(data);//вывожу на консоль
+
+            //    listener.Send(Encoding.UTF8.GetBytes("Все верно")); //кодируем и отправляем ответ listener
+
+            //    listener.Shutdown(SocketShutdown.Both); //выключил соединение и у клиента и у сервера
+            //    listener.Close(); //закрыл соединение(listener)
+            #endregion
+
             const string ip = "127.0.0.1"; //выбрал айпи, чтобы реализовывалось на одном компе
-            const int port = 8080;
+            const int port = 8081;
 
-            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);//сделал класс эндпоинт,котрый показывает куда смогут подключаться, сделал точку 
+            var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);//сделал переменную класса эндпоинт,котрый показывает куда смогут подключаться, сделал точку 
 
-            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//набор параметров для тсп протокола
-            tcpSocket.Bind(tcpEndPoint); //сказал сокету что ему слушать - режим ожидания
-            tcpSocket.Listen(2); //сколько клиентов может в очередь залезть
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);//набор параметров для udp протокола
+            udpSocket.Bind(udpEndPoint);
 
-            while (true) //пишу процесс бесконечного прослушивания
+            while (true)
             {
-                var listener = tcpSocket.Accept(); //для каждого клиента будет свой listener, который стирается, когда клиент получил всю запрашиваемую инфу
                 var buffer = new byte[256]; // байтовый массив, буфер
                 var size = 0; //сколько реально байт в сообщении получили
                 var data = new StringBuilder(); // будет собирать полученные данные
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);// создал экзэмпляр адреса эндпоинт, в который будет записан адрес клиента, который мы будем прослушивать
 
-                do //цикл с пост условием
+                do
                 {
-                    size = listener.Receive(buffer);
-                    data.Append(Encoding.UTF8.GetString(buffer, 0,size));//собственно, раскодирую полученные байты в куски сообщений. Использую кодировку UTF8
+                    size = udpSocket.ReceiveFrom(buffer, ref senderEndPoint);// ы предыдущей строке пришлось делаит ьприведение класса, потому что recieve только такое принимает
+                    data.Append(Encoding.UTF8.GetString(buffer));
                 }
-                while (listener.Available > 0); //считывает, пока есть данные
+                while (udpSocket.Available > 0);
+                
+                udpSocket.SendTo(Encoding.UTF8.GetBytes("Все получено"), senderEndPoint); //кодируем и отправляем ответ 
 
-                Console.WriteLine(data);//вывожу на консоль
-
-                listener.Send(Encoding.UTF8.GetBytes("Все верно")); //кодируем и отправляем ответ listener
-
-                listener.Shutdown(SocketShutdown.Both); //выключил соединение и у клиента и у сервера
-                listener.Close(); //закрыл соединение(listener)
+                Console.WriteLine(data);
             }
+
+
         }
     }
-}
+    }
+
